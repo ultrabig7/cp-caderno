f83fd4c29a727a51da33edfd1891d8e917961ecf
Lucas <lucsalmeidaoliveira@gmail.com>
2024-09-07 19:51:20 -0300

    Add files via upload

---
+++ Implementations/template.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using db = long double;
+
+#define ff first
+#define ss second
+#define pb push_back
+#define all(x) begin(x),end(x)
+
+#define FOR(i,a,b) for(int i = (a);i<(b);i++)
+#define F0R(i,a) FOR(i,0,a)
+#define rep(a) F0R(_,a)
+#define each(a,x) for(auto& a:x)
+#define sz(x) (int)size(x)
+#define vi vector<int>
+const int MOD = 1e9+7;
+const db PI = acos((db)-1);
+
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+
+    return 0;
+}
